fd5184b676c8bfcf96378ede616e7cf96e09baa3
QuintinAdam <quintinjadam@gmail.com>
2022-01-20 10:47:39 -0700

    adds devise and creates User

---
+++ app/models/user.rb
@@ -0,0 +1,52 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  admin                  :boolean
+#  confirmation_sent_at   :datetime
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :string
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  first_name             :string
+#  last_name              :string
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :string
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  sign_in_count          :integer          default(0), not null
+#  unconfirmed_email      :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_confirmation_token    (confirmation_token) UNIQUE
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable, :omniauthable
+
+  has_person_name
+
+  # ActiveStorage Associations
+  has_one_attached :avatar
+
+  # Associations
+
+  # We don't need users to confirm their email address on create,
+  # just when they change it
+  before_create :skip_confirmation!
+
+  # Validations
+  validates :name, presence: true
+  validates :avatar, resizable_image: true
+end
