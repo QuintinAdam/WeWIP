bf381a55bf4df81c62b7dd6854699dcb415c2b13
QuintinAdam <quintinjadam@gmail.com>
2022-01-22 20:42:45 -0700

    much better without that messy scaffold

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def posted_at(created_at)
+    # TODO: should make it work with the users timezone
+    if created_at.today?
+      "Today #{created_at.strftime('%B %e')}"
+    elsif created_at.yesterday?
+      "Yesterday #{created_at.strftime('%B %e')}"
+    else
+      "#{created_at.strftime('%B %e')}"
+    end
+  end
 end
