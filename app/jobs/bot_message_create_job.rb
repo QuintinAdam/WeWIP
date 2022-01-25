7f208e05624ecb0ac5743434f2efc34f9c20a115
QuintinAdam <quintinjadam@gmail.com>
2022-01-24 18:27:36 -0700

    ensure content on job

--- app/jobs/bot_message_create_job.rb
+++ app/jobs/bot_message_create_job.rb
@@ -2,7 +2,9 @@ class BotMessageCreateJob < ApplicationJob
   queue_as :default
 
   def perform(content, author)
-    bot = User.find_by_email('bot@bot.com')
-    bot.messages.create(content: content, author: author, project: Project.last)
+    if content.present?
+      bot = User.find_by_email('bot@bot.com')
+      bot.messages.create(content: content, author: author, project: Project.last)
+    end
   end
 end
